0743f638e20065c67f5f387993ec6032ffcf460a
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2021-04-27 08:42:55 -0300

    Separado sidebar e conteudo em componentes, arquivos de interfaces

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import Genre from "../interfaces/Genre";
+import { Button } from "./Button";
+
+interface SidebarProps {
+  genres: Genre[];
+  handleClickButton(id: number): void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
